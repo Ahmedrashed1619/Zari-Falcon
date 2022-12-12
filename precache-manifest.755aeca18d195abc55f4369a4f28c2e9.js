03a85706eb63adad23edf4b04d1df8c4c89d82ad
ahmedrashed1619 <ar9353062@gmail.com>
2022-12-12 21:30:43 +0200

    Updates

--- precache-manifest.755aeca18d195abc55f4369a4f28c2e9.js
+++ precache-manifest.755aeca18d195abc55f4369a4f28c2e9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "4e4d35156e9ebb7a6542",
-    "url": "/Zari-Falcon/static/css/main.e2dbb1b1.chunk.css"
+    "revision": "ed4fe7b83e722ae45e73",
+    "url": "/Zari-Falcon/static/css/main.b37ec87d.chunk.css"
   },
   {
-    "revision": "4e4d35156e9ebb7a6542",
-    "url": "/Zari-Falcon/static/js/main.4e4d3515.chunk.js"
+    "revision": "ed4fe7b83e722ae45e73",
+    "url": "/Zari-Falcon/static/js/main.ed4fe7b8.chunk.js"
   },
   {
     "revision": "a60f3433c35e0e88e173",
@@ -52,39 +52,39 @@ self.__precacheManifest = [
     "url": "/Zari-Falcon/static/media/signup.ebc1d025.png"
   },
   {
-    "revision": "4b162098158528431aeb5636116777f0",
-    "url": "/Zari-Falcon/static/media/fa-regular-400.4b162098.woff2"
-  },
-  {
-    "revision": "40e6df55734232692408d76ad5f9c34a",
-    "url": "/Zari-Falcon/static/media/fa-regular-400.40e6df55.ttf"
-  },
-  {
-    "revision": "6ddaf31a1e0e05490f6919cbb3347e9d",
-    "url": "/Zari-Falcon/static/media/fa-v4compatibility.6ddaf31a.woff2"
-  },
-  {
-    "revision": "0ef11ce096706bcafc520aee12162ae4",
-    "url": "/Zari-Falcon/static/media/fa-brands-400.0ef11ce0.ttf"
-  },
-  {
     "revision": "e2f5b365c7d3d4497da73148ddfae997",
     "url": "/Zari-Falcon/static/media/fa-brands-400.e2f5b365.woff2"
-  },
-  {
-    "revision": "c0bf087a238488d351270a35c8f0cca0",
-    "url": "/Zari-Falcon/static/media/fa-solid-900.c0bf087a.ttf"
   },
   {
     "revision": "328a9d0f59f0ebb55cddac6f39995bea",
     "url": "/Zari-Falcon/static/media/fa-solid-900.328a9d0f.woff2"
   },
   {
+    "revision": "4b162098158528431aeb5636116777f0",
+    "url": "/Zari-Falcon/static/media/fa-regular-400.4b162098.woff2"
+  },
+  {
+    "revision": "0ef11ce096706bcafc520aee12162ae4",
+    "url": "/Zari-Falcon/static/media/fa-brands-400.0ef11ce0.ttf"
+  },
+  {
+    "revision": "40e6df55734232692408d76ad5f9c34a",
+    "url": "/Zari-Falcon/static/media/fa-regular-400.40e6df55.ttf"
+  },
+  {
+    "revision": "c0bf087a238488d351270a35c8f0cca0",
+    "url": "/Zari-Falcon/static/media/fa-solid-900.c0bf087a.ttf"
+  },
+  {
     "revision": "36818ecc50af8ea6c4cbef88d0b2baad",
     "url": "/Zari-Falcon/static/media/fa-v4compatibility.36818ecc.ttf"
   },
   {
-    "revision": "02f664599c0eaab78ee13f431e841f40",
+    "revision": "6ddaf31a1e0e05490f6919cbb3347e9d",
+    "url": "/Zari-Falcon/static/media/fa-v4compatibility.6ddaf31a.woff2"
+  },
+  {
+    "revision": "8b37fae9f77f66321420b0b4b78d412a",
     "url": "/Zari-Falcon/index.html"
   }
 ];
